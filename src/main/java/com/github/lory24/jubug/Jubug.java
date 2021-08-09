bfad7c5a58ab12e9a215a17083315dcb4255c99c
LoRy24 <lorytherock24tutorial@gmail.com>
2021-08-09 17:13:18 +0200

    Updated eula check condition

--- src/main/java/com/github/lory24/jubug/Jubug.java
+++ src/main/java/com/github/lory24/jubug/Jubug.java
@@ -16,14 +16,24 @@ public class Jubug extends CraftServer {
     protected static void loadServer() {
         SERVER = new Jubug();
         System.out.println("Starting Jubug Server for Minecraft 1.8...");
+
+        // Files generation
+
+
+        // Eula check
         InputStream eulaStream = Jubug.class.getClassLoader().getResourceAsStream("eula.txt");
         assert eulaStream != null;
         BufferedReader bf = new BufferedReader(new InputStreamReader(eulaStream));
         StringBuilder eulaText = new StringBuilder();
         while (bf.ready()) eulaText.append(bf.readLine());
         ProprietiesSheet ps = ProprietiesReader.parseProprietiesFromString(eulaText.toString());
-        if (Boolean.parseBoolean(ps.getValues().get("name"))) getServer().runServer();
-        System.out.println("You have to accept the eula!");
+
+        if (!Boolean.parseBoolean(ps.getValues().get("name"))) {
+            System.out.println("You have to accept the eula!");
+            return;
+        }
+
+        getServer().runServer();
     }
 
     public static CraftServer getServer() {
