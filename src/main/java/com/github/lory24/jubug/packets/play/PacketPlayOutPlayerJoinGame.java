ef2a757cfdfc8da6e927e95ecbec7a63cf0fa2ca
LoRy24 <lorytherock24tutorial@gmail.com>
2021-09-05 19:50:19 +0200

    Updated the JoinGame packet

--- src/main/java/com/github/lory24/jubug/packets/play/PacketPlayOutPlayerJoinGame.java
+++ src/main/java/com/github/lory24/jubug/packets/play/PacketPlayOutPlayerJoinGame.java
@@ -1,22 +1,56 @@
 package com.github.lory24.jubug.packets.play;
 
 import com.github.lory24.jubug.game_utils.PlayerGameMode;
+import lombok.Getter;
 
 public class PacketPlayOutPlayerJoinGame extends PlayOutPacket {
-    private final int entityId;
-    private final boolean isHardcore;
-    private final PlayerGameMode gamemode; // can be -1
-    private final PlayerGameMode previousGamemode;
-    private final int worldCount;
+    @Getter private final int entityId;
+    @Getter private final boolean isHardcore;
+    @Getter private final PlayerGameMode gamemode;
+    @Getter private final PlayerGameMode previousGamemode; // can be -1
+    @Getter private final int worldCount;
+    @Getter private final String[] worldIdentifiers;
+    // dimension codec
+    // dimension
+    @Getter private final String worldName;
+    @Getter private final long hashedSeed;
+    @Getter private final int maxPlayers;
+    @Getter private final int viewDistance;
+    @Getter private final boolean reducedDebugInfo;
+    @Getter private final boolean enableRespawnScreen;
+    @Getter private final boolean isDebug;
+    @Getter private final boolean isFlat;
+    
 
-
-    public PacketPlayOutPlayerJoinGame(int entityId, boolean isHardcore, PlayerGameMode gamemode, PlayerGameMode previousGamemode, int worldCount) {
+    public PacketPlayOutPlayerJoinGame(int entityId,
+                                       boolean isHardcore,
+                                       PlayerGameMode gamemode,
+                                       PlayerGameMode previousGamemode,
+                                       int worldCount,
+                                       String[] worldIdentifiers,
+                                       String worldName,
+                                       long hashedSeed,
+                                       int maxPlayers,
+                                       int viewDistance,
+                                       boolean reducedDebugInfo,
+                                       boolean enableRespawnScreen,
+                                       boolean isDebug,
+                                       boolean isFlat) {
         super(0x26);
         this.entityId = entityId;
         this.isHardcore = isHardcore;
         this.gamemode = gamemode;
         this.previousGamemode = previousGamemode;
         this.worldCount = worldCount;
+        this.worldIdentifiers = worldIdentifiers;
+        this.worldName = worldName;
+        this.hashedSeed = hashedSeed;
+        this.maxPlayers = maxPlayers;
+        this.viewDistance = viewDistance;
+        this.reducedDebugInfo = reducedDebugInfo;
+        this.enableRespawnScreen = enableRespawnScreen;
+        this.isDebug = isDebug;
+        this.isFlat = isFlat;
     }
 
     @Override
